abf05902a575ddff4a28dbb012222a4461c50de9
Gianluca Campanella <gianluca@campanella.org>
2017-12-12 19:49:09 +0000

    Fixed style

--- 04_cpp_fundamentals_2/code/04_vectors.cpp
+++ 04_cpp_fundamentals_2/code/04_vectors.cpp
@@ -8,7 +8,7 @@ void print(const vector<int> & v)
     cout << "Elements: ";
     for (int i = 0; i < v.size(); i++)
     {
-        cout << v[i] << " ";  // [] doesn't check bounds
+        cout << v[i] << " "; // [] doesn't check bounds
     }
     cout << endl;
 }
